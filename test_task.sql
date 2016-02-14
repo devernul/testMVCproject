833d391d2105a4c230181ba22ef027552425e9f9
Dmitry <devernul@gmail.com>
2016-02-14 10:37:43 +0600

    add dumps MySql

---
+++ test_task.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.7.10-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `TASK_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DATE` date NOT NULL,
+  `DESCRIPTION` varchar(255) NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `STATUS_IS` int(11) NOT NULL,
+  PRIMARY KEY (`TASK_ID`),
+  KEY `FK272D859446A7D6` (`STATUS_IS`),
+  CONSTRAINT `FK272D859446A7D6` FOREIGN KEY (`STATUS_IS`) REFERENCES `status` (`STATUS_ID`),
+  CONSTRAINT `FKi9xagc4s0vsy3ohrfl6jtdwq3` FOREIGN KEY (`STATUS_IS`) REFERENCES `status` (`STATUS_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES (21,'2016-02-14','First Task','First',23),(22,'2016-02-14','2 task','2 task',22),(23,'2016-02-14','3 task','3 task',22),(24,'2016-02-14','4 task','4 task',22),(25,'2016-02-14','5 task','5 task',22),(26,'2016-02-14','6 task','6 task',22),(27,'2016-02-14','7 task','7 task',22),(28,'2016-02-14','8 task','8 task',22),(29,'2016-02-14','9 task','9 task',22),(30,'2016-02-14','10 task','10 task',22),(31,'2016-02-14','11 task','11 task',22),(32,'2016-02-14','12 task','12 task',22),(33,'2016-02-14','First Task','First',21);
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-14 10:32:33
